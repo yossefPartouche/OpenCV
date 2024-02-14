e3cd1dcbcdcc6fb8bf479c4cffc82e653a8be801
yossefpartouche <yossipartouche123@icloud.com>
2024-02-14 06:37:42 +0200

    image blur, Canny and dialation and Erosion image

---
+++ Chapter_3.py
@@ -0,0 +1,19 @@
+import cv2
+
+#Resizing and Cropping
+img = cv2.imread("/Users/yossipartouche/Desktop/Doc Photo/ISRAELI ID.jpeg")
+print(img.shape)
+#                                   height, width
+#   ---->
+#  |
+#  |
+#  v
+
+imgResize = cv2.resize(img, (500, 400))
+
+imgCropped = img[0:100, 100:200]
+
+#cv2.imshow("resized Image", imgResize)
+cv2.imshow("Cropped Image", imgCropped)
+#cv2.imshow("Image", img)
+cv2.waitKey(0)
